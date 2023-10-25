4df6464d371d143639f3a93fd4a0315eaba78a43
macielthiago <maciel.rosari54@gmail.com>
2023-10-25 18:32:24 -0400

    Variáveis

---
+++ Aula-02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
---
+++ Aula-03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula_03
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // int num = 0;
+            // char letra = 'a';
+            // float valor = 5.3f;
+            // string nome = "Thiago";
+
+            int num1, num2, res;
+
+            num1 = 10;
+            num2 = 25;
+
+            res = num1 + num2;
+        
+            Console.WriteLine("A soma de " + num1 + " mais " + num2 + " é igual a: " + res);
+
+        }
+    }
+}

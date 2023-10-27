07b306bf4fba1ca7b6593fc0187872a9c388e1bb
macielthiago <maciel.rosari54@gmail.com>
2023-10-27 18:38:09 -0400

    Operadores em C#

---
+++ Aula-04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula_04
+{
+    internal class Program
+    {
+         int num = 10;     // variável global
+
+        static void Main()
+        {
+            int num2 = 0;    // variável local
+            Console.WriteLine(num2);
+        }
+
+        private void Test()
+        {
+            Console.Write(num);
+        }
+    }
+}
---
+++ Aula-05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Operadores
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            int res = 10 + 5;
+            int num = 10;
+            num = num + 1;
+
+            bool res1 = 10 < 5;
+
+
+            Console.WriteLine(res);
+            Console.WriteLine(num);
+            Console.WriteLine(res1);
+
+            // operadores lógicos
+
+            // & = AND - E
+            // | = OR - OU
+        }
+    }
+}

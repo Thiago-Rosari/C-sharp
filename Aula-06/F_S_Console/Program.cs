efabda793bceac66b62792a2f6006206cc0d532c
macielthiago <maciel.rosari54@gmail.com>
2023-10-29 18:18:51 -0400

    Constantes e formatação

---
+++ Aula-06/F_S_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace F_S_Console
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n1, n2, n3;
+
+            n1 = 10; n2 = 20; n3 = 30;
+
+            Console.WriteLine("\nn1={0}\nn2={1}\nn3={2}\n", n1,n2,n3);  // uso de indicea
+
+
+            // \n = caractere de espaço
+            // \t = caractere de tabulação
+        }
+    }
+}
---
+++ Aula-06/Form_Especiais/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Form_Especiais
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double valCompra = 4.00;
+            double valVenda;
+            double lucro = 0.4;
+            string produto = "Pastel";
+
+            valVenda = valCompra + (valCompra * lucro);
+
+            Console.WriteLine("Produto........:{0,12}", produto);
+            Console.WriteLine("Val.Compra.....:{0,12:c}", valCompra);
+            Console.WriteLine("Lucro..........:{0,12:p}", lucro);
+            Console.WriteLine("Val.Venda......:{0,12:c}", valVenda);
+
+
+
+           
+            // {0 = indice da variável | 12 = espacamento | :c = valor monetario / :p = porcentagem}
+                    
+        }
+    }
+}
